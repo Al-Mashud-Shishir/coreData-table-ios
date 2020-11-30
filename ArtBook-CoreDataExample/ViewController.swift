c7440a2f6a8ba5eca1a312379510618674c9a63f
Md Abdullah Al Mashud <almashud137@gmail.com>
2020-11-30 16:14:19 +0600

    Initial Commit

---
+++ ArtBook-CoreDataExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArtBook-CoreDataExample
+//
+//  Created by Maddi on 30/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
